d13dbccdd9dee604b3ba8ac2126a51f1f34af3c8
miguel.minoldo <miguel.minoldo@valtech.com>
2022-06-20 18:20:32 +0200

    testing sugconf

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,11 +1,11 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
 export function middleware (req: NextRequest) {
-    console.log('UA: ' + req.headers.get('User-Agent'))
+    console.log('UA', req.headers.get('User-Agent'))
 
     const res = NextResponse.next()
 
-    res.headers.set('x-demo-this', '100')
+    res.headers.set('blabla', 'true')
 
     return res
 }
