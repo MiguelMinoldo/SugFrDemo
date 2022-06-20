655a3c7bbfdf4a16ed2c725fc604b411bd321cee
miguel.minoldo <miguel.minoldo@valtech.com>
2022-06-20 17:56:00 +0200

    commit from sugfr

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function  middleware(req: NextRequest) {
+export function middleware (req: NextRequest) {
     console.log('UA: ' + req.headers.get('User-Agent'))
 
     const res = NextResponse.next()
 
-    res.headers.set('x-something', 'This is middleware')
+    res.headers.set('x-demo-this', '100')
 
     return res
 }
